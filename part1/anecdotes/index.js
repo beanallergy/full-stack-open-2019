aaebaa474cee2a1352005afdfd1a9d111aed8409
beanallergy <13851786+beanallergy@users.noreply.github.com>
2019-06-18 17:57:51 +0300

    Exercise 1.13

--- part1/anecdotes/index.js
+++ part1/anecdotes/index.js
@@ -3,25 +3,47 @@ import ReactDOM from 'react-dom'
 
 const Button = ({text, handler}) => { 
   return (
-    <div>
-      <button onClick={handler()}> {text} </button>
-    </div>
+    <button onClick={handler()}> {text} </button>
   )
 }
 
-const App = (props) => {
+const VoteDisplay = (props) => { 
+  return (
+    <p> has {props.voteCount} votes </p>
+  )
+}
+
+const AnecdoteDisplay = (props) => { 
+  return (
+    <p> {props.anecdote} </p>
+  )
+}
+
+const App = ({anecdotes, startVotes}) => {
   const [selected, setSelected] = useState(0)
-  const selectRandom = () => {
+  const [votes, setVotes] = useState(startVotes)
+
+  const selectRandom = () => {  
     return() => {
       let value = Math.floor(Math.random() * 6)
       setSelected(value)  
     }
   }
 
+  const voteForSelected = () => {  
+    return() => {
+      let copy = [...votes]
+      copy[selected] += 1
+      setVotes(copy)
+    }
+  }
+
   return (
     <div>
-      {props.anecdotes[selected]}
-      <Button text='next anecdote' handler={selectRandom}/> 
+      <AnecdoteDisplay anecdote={anecdotes[selected]} />
+      <VoteDisplay voteCount={votes[selected]} />
+      <Button text='vote' handler={voteForSelected} /> 
+      <Button text='next anecdote' handler={selectRandom} /> 
     </div>
   )
 }
@@ -35,7 +57,9 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+const startVotes = Array(6).fill(0)
+
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotes={anecdotes} startVotes={startVotes} />,
   document.getElementById('root')
 )
