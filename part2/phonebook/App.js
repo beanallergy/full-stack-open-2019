f5f194324c6972c1f17a41da77980967e86d8750
beanallergy <13851786+beanallergy@users.noreply.github.com>
2019-06-20 20:08:09 +0300

    Exercise 2.9 - completed

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import People from './components/People'
 import NewPersonForm from './components/NewPersonForm'
-import FilterInput from './components/FilterInput'
+import NewInput from './components/NewInput'
 
 const Header = ({name}) => {
   return (
@@ -15,36 +15,30 @@ const App = (props) => {
     { name: 'Ada Loveart', number: '39-44-5323523' },
     { name: 'Dan Adamov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
 
-  const addNewPerson = (event) => {
-    event.preventDefault()
-    let names = persons.map(person => person['name'])
-    names.includes(newName)
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat({ 'name': newName , 'number': newNumber}))
+  const filterPeople = () => {
+    return ( 
+      persons.filter(person => 
+        person['name'].toString().toLowerCase()
+        .includes(filter.toString().toLowerCase())
+      )
+    )
   }
-
   
   return (
     <div>
       <Header name='Phonebook' />
-      <FilterInput label='filter shown with' filter={filter} setFilter={setFilter} list={persons} setNewList={setPersons} />
+      <NewInput label='filter shown with' value={filter} setNewValue={setFilter}/>
 
       <Header name='add a new' />
-      <form onSubmit={addNewPerson}>
-        <NewPersonForm label='name' newValue={newName} setNewValue={setNewName} />
-        <NewPersonForm label='number' newValue={newNumber} setNewValue={setNewNumber} />
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <NewPersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}/>
 
       <Header name='Numbers' />
-      <People people={persons} />
+      <People people={filterPeople()} />
     </div>
   )
 }
--- part2/phonebook/components/NewPersonForm.js
+++ part2/phonebook/components/NewPersonForm.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import NewInput from './NewInput'
 
-const NewPersonForm = ({label, newValue, setNewValue}) => {
-  const handleChange = (event) => {
-    setNewValue(event.target.value)
+const NewPersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNewNumber}) => {
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    let names = persons.map(person => person['name'])
+    names.includes(newName)
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat({ 'name': newName , 'number': newNumber}))
   }
 
   return (
-    <div>
-      {label}: <input value={newValue} onChange={handleChange}/>
-    </div>
+    <form onSubmit={addNewPerson}>
+      <NewInput label='name' newValue={newName} setNewValue={setNewName} />
+      <NewInput label='number' newValue={newNumber} setNewValue={setNewNumber} />
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
   )
 }
 export default NewPersonForm 
