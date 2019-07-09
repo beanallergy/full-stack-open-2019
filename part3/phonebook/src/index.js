4f90193194a1b158068b763cca1804bf8c850b04
beanallergy <13851786+beanallergy@users.noreply.github.com>
2019-07-09 12:10:08 +0300

    Exercises 3.1-3.2

---
+++ part3/phonebook/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.use(bodyParser.json())
+
+app.get('/info', (request, response) => {
+  const info_string = `Phonebook has info for ${persons.length} people`
+  response.send(`<p> ${info_string} </p> <p> ${Date()} </p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
