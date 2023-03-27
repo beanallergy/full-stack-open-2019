9bbb98c5ccf8fe058fb18ae79f4574ea9bbe2e66
Thao Nguyen <thaohuongams@gmail.com>
2023-03-27 19:17:39 +0300

    Exercises 1.12 - 1.14: anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import { useState } from 'react'
 
 const Header = ({title}) => {
   return (
@@ -22,26 +21,37 @@ const AnecdoteDisplay = ({anecdotes, votes, index}) => {
   )
 }
 
-const App = ({anecdotes, startVotes}) => {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const startVotes = Array(8).fill(0)
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(startVotes)
 
   const selectRandom = () => {  
     return() => {
-      let value = Math.floor(Math.random() * 6)
+      const value = Math.floor(Math.random() * 6)
       setSelected(value)  
     }
   }
 
   const voteForSelected = () => {
-    let copy = [...votes]
+    const copy = { ...votes }
     copy[selected] += 1
     return() => setVotes(copy)
   }
 
   const getMostVoteIndex = () => {
     let mostIndex = 0
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 8; i++) {
       if (votes[i] > votes[mostIndex]) { mostIndex=i }
     }
     return (mostIndex)
@@ -59,17 +69,4 @@ const App = ({anecdotes, startVotes}) => {
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-const startVotes = Array(6).fill(0)
-
-ReactDOM.render(
-  <App anecdotes={anecdotes} startVotes={startVotes} />,
-  document.getElementById('root')
-)
+export default App;
